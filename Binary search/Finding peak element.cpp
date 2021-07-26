4752a00127a80236346a1ecfaf6057ad55bb07b6
Pratik Jha <Pratikgns@gmail.com>
2021-07-26 16:29:43 +0530

    Update Finding peak element.cpp

--- Binary search/Finding peak element.cpp
+++ Binary search/Finding peak element.cpp
@@ -5,27 +5,6 @@ using namespace std;
 //finding peak element in a array
 
 
-/*
-int peak(vector<int> a,int n)
-{
-int f=0,l=n-1,mid;
-while(f<=l)
-{
-mid=(f+l)/2;
-if(mid>0&&mid<n-1)
-{if(a[mid]>a[mid-1]&&a[mid]>a[mid+1])
-return a[mid];
-}
-else {if(mid==0)
-if(a[mid+1]<a[mid])
-return a[mid];
-else if(mid==n-1)
-if(a[mid-1]<a[mid])
-return a[mid];
-}
-}
-}
-*/
 
 int peak(vector<int> a, int n)
 {
